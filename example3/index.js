da406dc6836888cc1c2d987a244e7a798dbeeb7a
SARVESH <sarveshtriphathi@gmail.com>
2019-05-30 23:13:56 +0530

    added example for 2.0 new features

--- example3/index.js
+++ example3/index.js
@@ -1,28 +1,43 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer, gql, ApolloError, ForbiddenError, AuthenticationError } = require("apollo-server");
 
 const typeDefs = gql`
   type Query {
-    hello: String
-    resolved: String
+    allTodos: String
+  }
+
+  type Mutation {
+    addTodo: String
   }
 `;
 
 const resolvers = {
   Query: {
-    resolved: () => 'Resolved',
-  },
-};
+    allTodos: (root, args, context) => {
+      if (!context.scope) {
+        throw new AuthenticationError("You must be logged in to see all todos");
+      }
 
-const mocks = {
-  Int: () => 6,
-  Float: () => 22.1,
-  String: () => 'Hello',
+      if (context.scope !== "ADMIN") {
+        throw new ForbiddenError("You must be an administrator to see all todos");
+      }
+
+      return context.Todos.getAllTodos();
+    },
+  },
+  Mutation: {
+    addTodo: (root, args, context) => {
+      if(!context.Todos.idAvailable(args.id)) {
+        throw new ApolloError('The id is already taken', 'DUPLICATE_KEY',  {field: 'id'});
+      }
+
+      return context.Todos.addTodo(args.id, args.todo);
+    }
+  }
 };
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
-  mocks,
+  resolvers
 });
 
 server.listen().then(({ url }) => {
