a5015a3d553fc70c2bbecb22e98ab308bc6bc316
SARVESH <sarveshtriphathi@gmail.com>
2019-05-30 23:17:15 +0530

    added example for 2.0 new features

---
+++ example4/index.js
@@ -0,0 +1,30 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+    resolved: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+     resolved: () => 'Resolved',
+  },
+};
+
+const mocks = {
+  Int: () => 6,
+  Float: () => 22.1,
+  String: () => 'Hello',
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  mocks,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
